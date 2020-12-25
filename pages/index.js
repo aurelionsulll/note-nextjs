c74dd0b674404f07d3bee61411181cc3f9005532
mehdi <mehdimn111@gmail.com>
2020-12-25 02:16:34 +0100

    sol

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-          <h1>test 1</h1>
+          <h1>test 101</h1>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
