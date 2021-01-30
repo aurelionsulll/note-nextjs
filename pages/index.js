6265f1d54e1af1728c055932f6a37499bc788e8e
mehdi <mehdimn111@gmail.com>
2021-01-30 18:18:04 +0100

    adding tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
+ import "tailwindcss/tailwind.css";
+ import Link from 'next/link';
+ import fetch from 'isomorphic-unfetch';
+ import "tailwindcss/tailwind.css";
+ 
+ const Index () => {
+   return ()
+ }
+ 
+ Index.getInitialProps = async () => {
+   const res = await fetch('http:://localhost:3000/api/notes');
+   const { data } = await res.json();
 
-const Index () => {
-  return ()
-}
-
-Index.getInitialProps = async () => {
-  
-}
+   return { notes: data }
+ }
